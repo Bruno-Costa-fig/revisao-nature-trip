1324aa372849eec2882e99e4f8b2bd3bd614ae64
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2024-09-26 20:33:36 -0300

    base aula

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,23 +1,21 @@
-import axios from 'axios';
+import axios from "axios"
 
-const api = axios.create({
-    baseURL: 'http://localhost:3000', 
-});
+const useAxios = axios.create({
+    baseURL: 'http://localhost:3000'
+})
 
-api.interceptors.request.use(
+useAxios.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token'); 
+        const token = localStorage.getItem('token')
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-            config.headers['Content-Type'] = 'application/json';
-        }
+        config.headers.Authorization = token
+        config.headers["Content-Type"] = 'application/json'
 
-        return config;
+        return config
     },
     (error) => {
         return Promise.reject(error);
     }
-);
+)
 
-export default api;
+export default useAxios
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,15 +7,19 @@ function Login(){
     const {register, handleSubmit, formState: {errors}, reset} = useForm()
 
     async function validarLogin(dados){
-       try {
-            const response = await axios.post("http://localhost:3000/login", dados)
+        try {
+            const response = await axios.post("http://localhost:3000/login", {
+                email: dados.email,
+                senha: dados.senha
+            })
 
-            if(!!response && response.status == 200){
-                const {token, user} = response.data
-                localStorage.setItem("token", token)
-                localStorage.setItem("user", JSON.stringify(user))
+            if(response.status == 200){
+                localStorage.setItem('token', response.data.token)
+                localStorage.setItem('user', JSON.stringify(response.data.user))
+
+                // redirect para o dashboard
             }
-       } catch (error) {
+        } catch (error){
             console.log(error)
         }
     }
