31aa1cae9ba67d66e954f73e49d3eb24c85c466c
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2024-10-03 20:33:52 -0300

    ajuste url api do prod

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', 
+    // baseURL: 'http://localhost:3000', 
+    baseURL: 'https://api-nature-trip-revisao-1.onrender.com', 
 });
 
 api.interceptors.request.use(
